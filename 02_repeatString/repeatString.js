6fa4c9b27a139d82f9665a7422750451a0a3a70c
Paulo Adeva <paulo.adeva@gmail.com>
2022-02-01 23:26:22 +1100

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+    try {
+        let something = string.repeat(num);
+        return something;
+    }
+    catch(err) {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
