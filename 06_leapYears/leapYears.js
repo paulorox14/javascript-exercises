928d6742136038b039a68828a6a4aba3464ad6ec
Paulo Adeva <paulo.adeva@gmail.com>
2022-02-06 22:35:21 +1100

    leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+     if (year % 4 == 0 && year % 100 != 0) {
+         return true;
+     }
+     else if (year % 400 == 0 && year % 100 == 0) {
+         return true;
+     }
+     else {
+         return false;
+     }
+}
 
-};
-
+console.log(leapYears(1996))
+console.log(leapYears(1997))
+console.log(leapYears(34992))
+console.log(leapYears(1900))
+console.log(leapYears(1600))
+console.log(leapYears(1700))
 // Do not edit below this line
 module.exports = leapYears;
