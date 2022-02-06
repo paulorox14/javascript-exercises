e2888675c0ef200f5183ee3b8cdc0a5909b23c1f
Paulo Adeva <paulo.adeva@gmail.com>
2022-02-06 22:06:42 +1100

    sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, remove) {
+    let index = list.indexOf(remove);
+    list.splice(index, 1);
+    return list;
 };
+
+console.log(removeFromArray([1,2,3,4], 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(start, final) {
+    if (typeof start != 'number' || typeof final != 'number') {
+        return "ERROR"
+    }
+    else if (start < 0 || final < 0) {
+        return "ERROR"
+    }
+    else {
+        let i = start;
+        let total = 0;
+        if (start < final) {
+            do {
+                total = total + start;
+                i ++;
+                start ++;
+            }
+            while (i <= final);
+        }
+        else if (start > final) {
+            do {
+                total = total + start;
+                i --;
+                start --;
+            }
+            while (i >= final);
+        }
 
-};
+        return total
+    }
+}
+
+console.log(sumAll(1, 4000))
+console.log(sumAll(123, 1))
+console.log(sumAll(1, "90"))
+console.log(sumAll(-10, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// 1 + 2 + 3 + 4 = 10
