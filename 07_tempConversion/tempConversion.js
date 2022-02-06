f4da1e445b7c344bb0376ecd466557b5900a145e
Paulo Adeva <paulo.adeva@gmail.com>
2022-02-07 00:15:43 +1100

    temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let fahrenheit = (num - 32) * 5 / 9;
+  let fahrenheitRounded = Math.round(fahrenheit * 10)/10;
+  return fahrenheitRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let celcius = num / 5 * 9 + 32;
+  let celciusRounded = Math.round(celcius * 10) / 10;
+  return celciusRounded;
 };
+
+console.log(ftoc(32))
+console.log(ctof(0))
+console.log(ftoc(-32))
 
 // Do not edit below this line
 module.exports = {
